fa5e2d310770849648a75aced6bc6d7432b8c254
Dilshodbek0720 <sayitqulovdilshodbek@gmail.com>
2024-04-30 23:32:32 +0500

    fixed

--- lib/ui/tab_box/history/widgets/create_folder_dialog.dart
+++ lib/ui/tab_box/history/widgets/create_folder_dialog.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:medify/data/local/storage_repository/storage_repository.dart';
+import 'package:medify/data/models/universal_data.dart';
+import 'package:medify/data/network/api_service.dart';
 import 'package:medify/utils/colors/app_colors.dart';
+import 'package:medify/utils/constants/storage_keys.dart';
 import 'package:medify/utils/size/size_extension.dart';
 
 void createFolderDialog({required BuildContext context}) {
@@ -53,19 +57,19 @@ void createFolderDialog({required BuildContext context}) {
                       hintText: "New folder",
                       enabledBorder: OutlineInputBorder(
                           borderSide: const BorderSide(color: AppColors.primary, width: 1.5),
-                          borderRadius: BorderRadius.circular(4.r),
+                          borderRadius: BorderRadius.circular(0),
                       ),
                       focusedBorder: OutlineInputBorder(
                         borderSide: const BorderSide(color: AppColors.primary, width: 1.5),
-                        borderRadius: BorderRadius.circular(4.r),
+                        borderRadius: BorderRadius.circular(0),
                       ),
                       errorBorder: OutlineInputBorder(
                         borderSide: const BorderSide(color: AppColors.primary, width: 1.5),
-                        borderRadius: BorderRadius.circular(4.r),
+                        borderRadius: BorderRadius.circular(0),
                       ),
                       border: OutlineInputBorder(
                         borderSide: const BorderSide(color: AppColors.primary, width: 1.5),
-                        borderRadius: BorderRadius.circular(4.r),
+                        borderRadius: BorderRadius.circular(0),
                       ),
                       fillColor: const Color(0xFFEFF2FB),
                       filled: true,
@@ -92,15 +96,22 @@ void createFolderDialog({required BuildContext context}) {
                 ),
               ),
               TextButton(
-                onPressed: folderName == '' ? null : (){
-
+                onPressed: ()async{
+                  ApiService apiService = ApiService();
+                  UniversalData data = await apiService.createNewFolder(token: StorageRepository.getString(StorageKeys.userToken), folderName: folderName);
+                  String message = data.data;
+                  if(message.isNotEmpty){
+                    print(message);
+                    print(StorageRepository.getString(StorageKeys.userToken));
+                  }
+                  Navigator.pop(context);
                 },
                 child: Text(
                   "Create",
                   style: TextStyle(
                       fontFamily: "Raleway",
                       fontSize: 16.sp,
-                      color: folderName == '' ? AppColors.primary400.withOpacity(0.8) : AppColors.primary
+                      color: AppColors.primary
                   ),
                 ),
               ),
